c0c39986f444d2594433cdaf16574dae681fb8b3
jiandong <jiandong.c@gmail.com>
2016-01-02 22:43:08 +0000

    add more protractor e2e tests

--- angular-app/e2e-tests/shopping_cart.js
+++ angular-app/e2e-tests/shopping_cart.js
@@ -4,6 +4,7 @@ describe('shopping carts', function() {
 
     beforeEach(function() {
         browser.get('http://localhost:8000/app/#/products');
+
     });
 
     it('should display all products', function() {
@@ -11,4 +12,12 @@ describe('shopping carts', function() {
         expect(items.count()).toBe(20);
     });
 
+    it('should be able to add items in shopping cart as anonymous user', function() {
+        browser.ignoreSynchronization = true;
+        element.all(by.repeater('product in products')).get(0).$$('button').click();
+        expect(element(by.css('.nav.navbar-nav.navbar-right')).$$('li').get(2).getText()).toEqual('1 items - £10');
+        element.all(by.repeater('product in products')).get(1).$$('button').click();
+        expect(element(by.css('.nav.navbar-nav.navbar-right')).$$('li').get(2).getText()).toEqual('2 items - £30');
+
+    });
 });
--- scripts/stall.sh
+++ scripts/stall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # start all services
 
-java -jar ../customer/target/customer-1.0-SNAPSHOT.jar & 
-java -jar ../product/target/product-1.0-SNAPSHOT.jar &
-java -jar ../order/target/order-1.0-SNAPSHOT.jar &
-java -jar ../authserver/target/authserver-1.0-SNAPSHOT.jar &
+java -Xmx256m -jar ../customer/target/customer-1.0-SNAPSHOT.jar & 
+java -Xmx256m -jar ../product/target/product-1.0-SNAPSHOT.jar &
+java -Xmx256m -jar ../order/target/order-1.0-SNAPSHOT.jar &
+java -Xmx256m -jar ../authserver/target/authserver-1.0-SNAPSHOT.jar &
