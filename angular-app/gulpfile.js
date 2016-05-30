b5e5c738fc96f7a78c572ac9d3c6f1789875ab0b
jiandong <jiandong.c@gmail.com>
2016-05-30 23:03:04 +0100

    config multiple environments

--- angular-app/gulpfile.js
+++ angular-app/gulpfile.js
@@ -1,7 +1,13 @@
 var gulp = require('gulp'), gulpNgConfig = require('gulp-ng-config');
 
-gulp.task('config', function () {
+gulp.task('configDev', function () {
   gulp.src('config.json')
-  .pipe(gulpNgConfig('config'))
+  .pipe(gulpNgConfig('config', {environment: 'local'}))
+  .pipe(gulp.dest('app/'));
+});
+
+gulp.task('configProd', function () {
+  gulp.src('config.json')
+  .pipe(gulpNgConfig('config', {environment: 'production'}))
   .pipe(gulp.dest('app/'));
 });
