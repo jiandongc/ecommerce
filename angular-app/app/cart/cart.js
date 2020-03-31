5f209027984b041f401bdc260873af76bb64743b
jiandongchen <jiandong.c@gmail.com>
2020-03-31 23:43:48 +0100

    ui: crete shopping cart info with customerid and email

--- angular-app/app/cart/cart.js
+++ angular-app/app/cart/cart.js
@@ -1,157 +1,157 @@
-var cart = angular.module('cart',[]);
+var cart = angular.module('cart', []);
 
 cart.controller('cartCtrl', function($scope, $rootScope, shoppingCartFactory, $localstorage, $timeout, $location) {
 
-  $scope.template.header = 'cart-header.html';
-  $scope.template.footer = 'default-footer.html';
+    $scope.template.header = 'cart-header.html';
+    $scope.template.footer = 'default-footer.html';
 
-  $scope.updateItem = function(cartItem){
-    if(cartItem.quantity == undefined){
-        $timeout(function() {
-          $rootScope.$broadcast('updateCartSummary', false);
-        }, 3000);
-    } else {
-        var cartUid = $localstorage.get('cart_uid');
-        shoppingCartFactory.updateShoppingCartItem(cartUid, cartItem).then(function(response){
-            $rootScope.$broadcast('updateCartSummary', false);
-        });
+    $scope.updateItem = function(cartItem) {
+        if (cartItem.quantity == undefined) {
+            $timeout(function() {
+                $rootScope.$broadcast('updateCartSummary', false);
+            }, 3000);
+        } else {
+            var cartUid = $localstorage.get('cart_uid');
+            shoppingCartFactory.updateShoppingCartItem(cartUid, cartItem).then(function(response) {
+                $rootScope.$broadcast('updateCartSummary', false);
+            });
+        }
     }
-  }
 
-	$scope.checkout = function(){
-		if(typeof $localstorage.get('cart_uid', undefined) !== "undefined") {
-			$location.path("/shipping");
-		} else {
-			$location.path("/login");
-		}
-	};
+    $scope.checkout = function() {
+        if (typeof $localstorage.get('cart_uid', undefined) !== "undefined") {
+            $location.path("/shipping");
+        } else {
+            $location.path("/login");
+        }
+    };
 });
 
-cart.factory('shoppingCartFactory', function($http, environment){
-  var addCustomerInfo = function(cartUid, customerData){
-    return $http.put(environment.shoppingCartUrl + '/carts/' + cartUid, customerData).then(function(response){
-      return response.data;
-    });
-  }
+cart.factory('shoppingCartFactory', function($http, environment) {
+    var addCustomerInfo = function(cartUid, customerData) {
+        return $http.put(environment.shoppingCartUrl + '/carts/' + cartUid, customerData).then(function(response) {
+            return response.data;
+        });
+    }
 
-  var updateEmail = function(cartUid, email){
-    return $http.put(environment.shoppingCartUrl + '/carts/' + cartUid + "/email", email).then(function(response){
-      return response.data;
-    });
-  }
+    var updateEmail = function(cartUid, email) {
+        return $http.put(environment.shoppingCartUrl + '/carts/' + cartUid + "/email", email).then(function(response) {
+            return response.data;
+        });
+    }
 
-  var createShoppingCartForCustomer = function(customerId){
-    return $http.post(environment.shoppingCartUrl + '/carts', customerId).then(function(response){
-        return response.data;
-    }); 
-  }
+    var createShoppingCartForCustomer = function(customerData) {
+        return $http.post(environment.shoppingCartUrl + '/carts', customerData).then(function(response) {
+            return response.data;
+        });
+    }
 
-  var createShoppingCartForGuest = function(){
-    return $http.post(environment.shoppingCartUrl + '/carts').then(function(response){
-        return response.data;
-    }); 
-  }
+    var createShoppingCartForGuest = function() {
+        return $http.post(environment.shoppingCartUrl + '/carts').then(function(response) {
+            return response.data;
+        });
+    }
 
-  var addItemToShoppingCart = function(name, code, price, imageUrl, sku, description, cartUid){
-    var cartItem = {
-      name: name, 
-      code: code,
-      price: price,
-      imageUrl: imageUrl,
-      sku: sku,
-      description: description
+    var addItemToShoppingCart = function(name, code, price, imageUrl, sku, description, cartUid) {
+        var cartItem = {
+            name: name,
+            code: code,
+            price: price,
+            imageUrl: imageUrl,
+            sku: sku,
+            description: description
+        };
+
+        return $http.post(environment.shoppingCartUrl + '/carts/' + cartUid + '/items', cartItem).then(function(response) {
+            return response.data;
+        });
+    }
+
+    var getShoppingCart = function(cartUid) {
+        return $http.get(environment.shoppingCartUrl + '/carts/' + cartUid).then(function(response) {
+            return response.data;
+        });
+    }
+
+    var getShoppingCartByCustomerId = function(customerId) {
+        return $http.get(environment.shoppingCartUrl + '/carts?customerId=' + customerId).then(function(response) {
+            return response.data;
+        });
+    }
+
+    var deleteItemFromShoppingCart = function(cartUid, sku) {
+        return $http.delete(environment.shoppingCartUrl + '/carts/' + cartUid + '/items/' + sku).then(function(response) {
+            return response.data;
+        });
+    }
+
+    var updateShoppingCartItem = function(cartUid, cartItem) {
+        return $http.put(environment.shoppingCartUrl + '/carts/' + cartUid + '/items', cartItem).then(function(response) {
+            return response.data;
+        });
+    }
+
+    var addAddress = function(cartUid, address) {
+        return $http.post(environment.shoppingCartUrl + '/carts/' + cartUid + '/addresses', address).then(function(response) {
+            return response.data;
+        });
     };
 
-    return $http.post(environment.shoppingCartUrl + '/carts/' + cartUid + '/items', cartItem).then(function(response){
-        return response.data;
-    });
-  }
+    var addDeliveryOption = function(cartUid, deliveryOption) {
+        return $http.post(environment.shoppingCartUrl + '/carts/' + cartUid + '/deliveryoption', deliveryOption).then(function(response) {
+            return response.data;
+        });
+    };
 
-  var getShoppingCart = function(cartUid){
-      return $http.get(environment.shoppingCartUrl + '/carts/' + cartUid).then(function(response){
-          return response.data;
-      });   
-  }
+    var getDeliveryOption = function(cartUid) {
+        return $http.get(environment.shoppingCartUrl + '/carts/' + cartUid + '/deliveryoption').then(function(response) {
+            return response.data;
+        });
+    };
 
-  var getShoppingCartByCustomerId = function(customerId){
-      return $http.get(environment.shoppingCartUrl + '/carts?customerId=' + customerId).then(function(response){
-          return response.data;
-      });
-  }
+    var getSageMerchantKey = function() {
+        return $http.get(environment.shoppingCartUrl + '/carts/sage').then(function(response) {
+            return response.data;
+        });
+    };
 
-  var deleteItemFromShoppingCart = function(cartUid, sku){
-      return $http.delete(environment.shoppingCartUrl + '/carts/' + cartUid + '/items/' + sku).then(function(response){
-          return response.data;
-      }); 
-  }
+    var submitTrnsactionToSage = function(cartUid, card) {
+        return $http.post(environment.shoppingCartUrl + '/carts/sage/transactions/' + cartUid, card).then(function(response) {
+            return response.data;
+        });
+    };
 
-  var updateShoppingCartItem = function(cartUid, cartItem){
-      return $http.put(environment.shoppingCartUrl + '/carts/' + cartUid + '/items', cartItem).then(function(response){
-        return response.data;
-      });   
-  }
+    var getOrderData = function(cartUid) {
+        return $http.get(environment.shoppingCartUrl + '/carts/order-data/' + cartUid).then(function(response) {
+            return response.data;
+        });
+    };
 
-  var addAddress = function(cartUid, address) {
-      return $http.post(environment.shoppingCartUrl + '/carts/' + cartUid + '/addresses', address).then(function(response){
-        return response.data;
-      }); 
-  };
-
-  var addDeliveryOption = function(cartUid, deliveryOption) {
-      return $http.post(environment.shoppingCartUrl + '/carts/' + cartUid + '/deliveryoption', deliveryOption).then(function(response){
-        return response.data;
-      }); 
-  };
-
-  var getDeliveryOption = function(cartUid) {
-      return $http.get(environment.shoppingCartUrl + '/carts/' + cartUid + '/deliveryoption').then(function(response){
-        return response.data;
-      }); 
-  };
-
-  var getSageMerchantKey = function() {
-      return $http.get(environment.shoppingCartUrl + '/carts/sage').then(function(response){
-        return response.data;
-      }); 
-  };
-
-  var submitTrnsactionToSage = function(cartUid, card){
-      return $http.post(environment.shoppingCartUrl + '/carts/sage/transactions/' + cartUid, card).then(function(response){
-        return response.data;
-      }); 
-  };
-
-  var getOrderData = function(cartUid){
-      return $http.get(environment.shoppingCartUrl + '/carts/order-data/' + cartUid).then(function(response){
-        return response.data;
-      }); 
-  };
-
-  return {
-    addCustomerInfo: addCustomerInfo,
-    updateEmail: updateEmail,
-    addItemToShoppingCart: addItemToShoppingCart,
-    createShoppingCartForCustomer: createShoppingCartForCustomer,
-    createShoppingCartForGuest: createShoppingCartForGuest,
-    getShoppingCart: getShoppingCart,
-    getShoppingCartByCustomerId: getShoppingCartByCustomerId,
-    deleteItemFromShoppingCart: deleteItemFromShoppingCart,
-    updateShoppingCartItem: updateShoppingCartItem,
-    addAddress: addAddress,
-    addDeliveryOption: addDeliveryOption,
-    getDeliveryOption: getDeliveryOption,
-    getSageMerchantKey: getSageMerchantKey,
-    submitTrnsactionToSage: submitTrnsactionToSage,
-    getOrderData: getOrderData
-  }
+    return {
+        addCustomerInfo: addCustomerInfo,
+        updateEmail: updateEmail,
+        addItemToShoppingCart: addItemToShoppingCart,
+        createShoppingCartForCustomer: createShoppingCartForCustomer,
+        createShoppingCartForGuest: createShoppingCartForGuest,
+        getShoppingCart: getShoppingCart,
+        getShoppingCartByCustomerId: getShoppingCartByCustomerId,
+        deleteItemFromShoppingCart: deleteItemFromShoppingCart,
+        updateShoppingCartItem: updateShoppingCartItem,
+        addAddress: addAddress,
+        addDeliveryOption: addDeliveryOption,
+        getDeliveryOption: getDeliveryOption,
+        getSageMerchantKey: getSageMerchantKey,
+        submitTrnsactionToSage: submitTrnsactionToSage,
+        getOrderData: getOrderData
+    }
 });
 
 cart.config(
-  function($routeProvider) {
-    $routeProvider.
-      when('/cart', {
-        templateUrl: 'cart/cart.html',
-        controller: 'cartCtrl'
-      });
+    function($routeProvider) {
+        $routeProvider.
+        when('/cart', {
+            templateUrl: 'cart/cart.html',
+            controller: 'cartCtrl'
+        });
 
-});
+    });
--- angular-app/app/listing/product.js
+++ angular-app/app/listing/product.js
@@ -1,223 +1,226 @@
 var productDetail = angular.module('productDetail', ['ngRoute']);
 
 productDetail.controller('productDetailCtrl', function($scope, $rootScope, $localstorage, $routeParams, $timeout, productFactory, shoppingCartFactory, customerFactory, categoryFactory) {
-  $scope.addingItem = false;
-  $scope.selectOptionAlert = false;
-  $scope.addingItemToFavourites = false;
-  $scope.addedToFavouriteAlert = false;
-  $scope.loading = true;
-  $scope.customerId = $localstorage.get("customer_id");
+    $scope.addingItem = false;
+    $scope.selectOptionAlert = false;
+    $scope.addingItemToFavourites = false;
+    $scope.addedToFavouriteAlert = false;
+    $scope.loading = true;
+    $scope.customerId = $localstorage.get("customer_id");
 
-	productFactory.getProductWithCode($routeParams.code).then(function(response){
-		$scope.product = response;
-    $scope.loading = false;
+    productFactory.getProductWithCode($routeParams.code).then(function(response) {
+        $scope.product = response;
+        $scope.loading = false;
 
-    categoryFactory.getCategoryData(response.categoryCode).then(function(response){
-      $scope.parentcategories = response.parents;
-    });
+        categoryFactory.getCategoryData(response.categoryCode).then(function(response) {
+            $scope.parentcategories = response.parents;
+        });
 
-    $scope.price = response.price;
-    $scope.originalPrice = response.originalPrice;
-    $scope.onSale = response.onSale;
-    $scope.discountRate = response.discountRate;
-    
-    if($scope.product.variants.length == 1){
-      var variant = $scope.product.variants[0];
-      $scope.sku = variant.sku;
-      $scope.description = variant.description;
-      $scope.qty = variant.qty;
-    } else {
-      $scope.sku = undefined;
-      $scope.description = undefined;
-      $scope.qty = undefined;
-    }
+        $scope.price = response.price;
+        $scope.originalPrice = response.originalPrice;
+        $scope.onSale = response.onSale;
+        $scope.discountRate = response.discountRate;
 
-    $scope.selected = {};
-    for(var attribute in $scope.product.attributes){
-      $scope.selected[attribute] = '';
-    }
-	});
-
-  productFactory.getProductWithColorVariants($routeParams.code).then(function(response){
-    $scope.colorVariant = response;
-  });
-
-  $scope.select=function(attribute, value){
-    $scope.selected[attribute] = value;
-    var variant = $scope.find();
-
-    if(variant){
-      $scope.price = variant.price;
-      $scope.originalPrice = variant.originalPrice;
-      $scope.onSale = variant.isOnSale;
-      $scope.discountRate = variant.discountRate;
-      $scope.sku = variant.sku;
-      $scope.description = variant.description;
-      $scope.qty = variant.qty;
-      $scope.selectOptionAlert = false;
-    } else {
-      $scope.price = $scope.product.price;
-      $scope.originalPrice = $scope.product.originalPrice;
-      $scope.onSale = $scope.product.onSale;
-      $scope.discountRate = $scope.product.discountRate;
-      $scope.sku = undefined;
-      $scope.description = undefined;
-      $scope.qty = undefined;
-      $scope.selectOptionAlert = true;
-    }
-  };
-
-  $scope.find=function(){
-    outter:
-    for(var i in $scope.product.variants){
-      var variant = $scope.product.variants[i];
-      for(var attribute in $scope.selected){
-        if(variant[attribute] != $scope.selected[attribute]){
-          continue outter;
+        if ($scope.product.variants.length == 1) {
+            var variant = $scope.product.variants[0];
+            $scope.sku = variant.sku;
+            $scope.description = variant.description;
+            $scope.qty = variant.qty;
+        } else {
+            $scope.sku = undefined;
+            $scope.description = undefined;
+            $scope.qty = undefined;
         }
-      }
-      return variant;
+
+        $scope.selected = {};
+        for (var attribute in $scope.product.attributes) {
+            $scope.selected[attribute] = '';
+        }
+    });
+
+    productFactory.getProductWithColorVariants($routeParams.code).then(function(response) {
+        $scope.colorVariant = response;
+    });
+
+    $scope.select = function(attribute, value) {
+        $scope.selected[attribute] = value;
+        var variant = $scope.find();
+
+        if (variant) {
+            $scope.price = variant.price;
+            $scope.originalPrice = variant.originalPrice;
+            $scope.onSale = variant.isOnSale;
+            $scope.discountRate = variant.discountRate;
+            $scope.sku = variant.sku;
+            $scope.description = variant.description;
+            $scope.qty = variant.qty;
+            $scope.selectOptionAlert = false;
+        } else {
+            $scope.price = $scope.product.price;
+            $scope.originalPrice = $scope.product.originalPrice;
+            $scope.onSale = $scope.product.onSale;
+            $scope.discountRate = $scope.product.discountRate;
+            $scope.sku = undefined;
+            $scope.description = undefined;
+            $scope.qty = undefined;
+            $scope.selectOptionAlert = true;
+        }
+    };
+
+    $scope.find = function() {
+        outter: for (var i in $scope.product.variants) {
+            var variant = $scope.product.variants[i];
+            for (var attribute in $scope.selected) {
+                if (variant[attribute] != $scope.selected[attribute]) {
+                    continue outter;
+                }
+            }
+            return variant;
+        }
+        return null;
+    };
+
+    $scope.addItemToCart = function(product) {
+
+        if (typeof $scope.sku === "undefined" || typeof $scope.price === "undefined") {
+            $scope.selectOptionAlert = true;
+            return;
+        }
+
+        var imageUrl = product.images[0];
+        $scope.addingItem = true;
+        if (!$localstorage.containsKey("cart_uid")) {
+            if ($localstorage.containsKey("customer_id")) {
+                customerFactory.getCustomerById($localstorage.get("customer_id")).then(function(customerData) {
+                    shoppingCartFactory.createShoppingCartForCustomer(customerData).then(function(cartUid) {
+                        shoppingCartFactory.addItemToShoppingCart(product.name, product.code, $scope.price, imageUrl, $scope.sku, $scope.description, cartUid).then(function(data) {
+                            $localstorage.set('cart_uid', cartUid);
+                            $rootScope.$broadcast('updateCartSummary', true);
+                            $scope.addingItem = false;
+                        });
+                    });
+
+                });
+            } else {
+                shoppingCartFactory.createShoppingCartForGuest().then(function(cartUid) {
+                    shoppingCartFactory.addItemToShoppingCart(product.name, product.code, $scope.price, imageUrl, $scope.sku, $scope.description, cartUid).then(function(data) {
+                        $localstorage.set('cart_uid', cartUid);
+                        $rootScope.$broadcast('updateCartSummary', true);
+                        $scope.addingItem = false;
+                    });
+                });
+            }
+        } else {
+            var cartUid = $localstorage.get('cart_uid', undefined);
+            shoppingCartFactory.addItemToShoppingCart(product.name, product.code, $scope.price, imageUrl, $scope.sku, $scope.description, cartUid).then(function(data) {
+                $rootScope.$broadcast('updateCartSummary', true);
+                $scope.addingItem = false;
+            });
+        }
+
+    };
+
+    $scope.addToFavourite = function(product) {
+        $scope.addingItemToFavourites = true;
+        customerFactory.addToFavourite($scope.customerId, product.code).then(function(product) {
+            $scope.addingItemToFavourites = false;
+            $scope.addedToFavouriteAlert = true;
+        });
     }
-    return null;
-  };
-
-  $scope.addItemToCart = function(product){
-
-      if(typeof $scope.sku === "undefined" || typeof $scope.price === "undefined"){
-        $scope.selectOptionAlert = true;
-        return;
-      }
-
-      var imageUrl = product.images[0];
-      $scope.addingItem = true;
-      if(!$localstorage.containsKey("cart_uid")){
-          if($localstorage.containsKey("customer_id")){
-              shoppingCartFactory.createShoppingCartForCustomer($localstorage.get("customer_id")).then(function(cartUid){
-                  shoppingCartFactory.addItemToShoppingCart(product.name, product.code, $scope.price, imageUrl, $scope.sku, $scope.description, cartUid).then(function(data){
-                      $localstorage.set('cart_uid', cartUid);
-                      $rootScope.$broadcast('updateCartSummary', true);
-                      $scope.addingItem = false;
-                  });
-              }); 
-          } else {
-              shoppingCartFactory.createShoppingCartForGuest().then(function(cartUid){
-                  shoppingCartFactory.addItemToShoppingCart(product.name, product.code, $scope.price, imageUrl, $scope.sku, $scope.description, cartUid).then(function(data){
-                      $localstorage.set('cart_uid', cartUid);
-                      $rootScope.$broadcast('updateCartSummary', true);
-                      $scope.addingItem = false;
-                  });
-              }); 
-          }
-      } else {
-          var cartUid = $localstorage.get('cart_uid', undefined);
-          shoppingCartFactory.addItemToShoppingCart(product.name, product.code, $scope.price, imageUrl, $scope.sku, $scope.description, cartUid).then(function(data){
-              $rootScope.$broadcast('updateCartSummary', true);
-              $scope.addingItem = false;
-          });  
-      }
-
-  };
-
-  $scope.addToFavourite = function(product){
-    $scope.addingItemToFavourites = true;
-    customerFactory.addToFavourite($scope.customerId, product.code).then(function(product){
-      $scope.addingItemToFavourites = false;
-      $scope.addedToFavouriteAlert = true;
-    });
-  }
 });
 
-productDetail.factory('productFactory', function($http, environment){
-  var getProductWithCode = function(productCode){
-    return $http.get(environment.productUrl + '/products/' + productCode).then(function(response){
-      return response.data;
-    });
-  }
+productDetail.factory('productFactory', function($http, environment) {
+    var getProductWithCode = function(productCode) {
+        return $http.get(environment.productUrl + '/products/' + productCode).then(function(response) {
+            return response.data;
+        });
+    }
 
-  var getProductWithColorVariants = function(productCode){
-    return $http.get(environment.productUrl + '/products/color/' + productCode).then(function(response){
-      return response.data;
-      $scope.colorVariant = response.data;
-    });
-  }
+    var getProductWithColorVariants = function(productCode) {
+        return $http.get(environment.productUrl + '/products/color/' + productCode).then(function(response) {
+            return response.data;
+            $scope.colorVariant = response.data;
+        });
+    }
 
-  var getProductsWithTag = function(tag){
-    return $http.get(environment.productUrl + '/products?tg=' + tag).then(function(response){
-      return response.data;
-    });
-  }
+    var getProductsWithTag = function(tag) {
+        return $http.get(environment.productUrl + '/products?tg=' + tag).then(function(response) {
+            return response.data;
+        });
+    }
 
-  var getProductsWithTagInCategory = function(tag, categoryCode){
-    return $http.get(environment.productUrl + '/products?tg=' + tag + '&cc=' + categoryCode).then(function(response){
-      return response.data;
-    });
-  }
+    var getProductsWithTagInCategory = function(tag, categoryCode) {
+        return $http.get(environment.productUrl + '/products?tg=' + tag + '&cc=' + categoryCode).then(function(response) {
+            return response.data;
+        });
+    }
 
-  var getProductsWithTagInOrder = function(tag, sort){
-    return $http.get(environment.productUrl + '/products?tg=' + tag + '&sort=' + sort).then(function(response){
-      return response.data;
-    });
-  }
+    var getProductsWithTagInOrder = function(tag, sort) {
+        return $http.get(environment.productUrl + '/products?tg=' + tag + '&sort=' + sort).then(function(response) {
+            return response.data;
+        });
+    }
 
-  var getProductsWithBrand = function(brand){
-    return $http.get(environment.productUrl + '/products?br=' + brand).then(function(response){
-      return response.data;
-    });
-  }
+    var getProductsWithBrand = function(brand) {
+        return $http.get(environment.productUrl + '/products?br=' + brand).then(function(response) {
+            return response.data;
+        });
+    }
 
-  var getProductsWithBrandInOrder = function(brand, sort){
-    return $http.get(environment.productUrl + '/products?br=' + brand + '&sort=' + sort).then(function(response){
-      return response.data;
-    });
-  }
+    var getProductsWithBrandInOrder = function(brand, sort) {
+        return $http.get(environment.productUrl + '/products?br=' + brand + '&sort=' + sort).then(function(response) {
+            return response.data;
+        });
+    }
 
 
-  return {
-    getProductWithCode: getProductWithCode,
-    getProductWithColorVariants: getProductWithColorVariants,
-    getProductsWithTag: getProductsWithTag,
-    getProductsWithTagInCategory: getProductsWithTagInCategory,
-    getProductsWithTagInOrder: getProductsWithTagInOrder,
-    getProductsWithBrand: getProductsWithBrand,
-    getProductsWithBrandInOrder: getProductsWithBrandInOrder
-  }
+    return {
+        getProductWithCode: getProductWithCode,
+        getProductWithColorVariants: getProductWithColorVariants,
+        getProductsWithTag: getProductsWithTag,
+        getProductsWithTagInCategory: getProductsWithTagInCategory,
+        getProductsWithTagInOrder: getProductsWithTagInOrder,
+        getProductsWithBrand: getProductsWithBrand,
+        getProductsWithBrandInOrder: getProductsWithBrandInOrder
+    }
 });
 
-productDetail.directive('onFinishInitSlider', function ($timeout) {
+productDetail.directive('onFinishInitSlider', function($timeout) {
     return {
         restrict: 'A',
-        link: function (scope, element, attr) {
-          if (scope.$last === true) {
-            $timeout(function () {
-              $('.flexslider').flexslider({
-                animation: 'slide', 
-                controlNav: 'thumbnails',
-                slideshow: false
-              });
-            });
-          }
+        link: function(scope, element, attr) {
+            if (scope.$last === true) {
+                $timeout(function() {
+                    $('.flexslider').flexslider({
+                        animation: 'slide',
+                        controlNav: 'thumbnails',
+                        slideshow: false
+                    });
+                });
+            }
         }
     }
 });
 
-productDetail.directive('dynamic', function ($compile) {
-  return {
-    restrict: 'A',
-    replace: true,
-    link: function (scope, ele, attrs) {
-      scope.$watch(attrs.dynamic, function(html) {
-        ele.html(html);
-        $compile(ele.contents())(scope);
-      });
-    }
-  };
+productDetail.directive('dynamic', function($compile) {
+    return {
+        restrict: 'A',
+        replace: true,
+        link: function(scope, ele, attrs) {
+            scope.$watch(attrs.dynamic, function(html) {
+                ele.html(html);
+                $compile(ele.contents())(scope);
+            });
+        }
+    };
 });
 
 productDetail.config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider.
-      when('/products/:code', {
-        templateUrl: 'listing/product.html',
-        controller: 'productDetailCtrl'
-      });
-  }]);
+    function($routeProvider) {
+        $routeProvider.
+        when('/products/:code', {
+            templateUrl: 'listing/product.html',
+            controller: 'productDetailCtrl'
+        });
+    }
+]);
