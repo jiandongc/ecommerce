9dcc3f97c83239d2ddcb434f14c99cc54fe87056
jiandongc <jiandong.c@gmail.com>
2020-10-10 23:52:46 +0100

    authentication: admin token

--- authentication/src/main/java/authserver/UserDetailServiceImpl.java
+++ authentication/src/main/java/authserver/UserDetailServiceImpl.java
@@ -1,6 +1,7 @@
 package authserver;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -8,23 +9,33 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.springframework.security.core.authority.AuthorityUtils.createAuthorityList;
 
 @Service
 public class UserDetailServiceImpl implements UserDetailsService {
 
     private final UserRepository userRepository;
+    private final List<String> admins;
 
     @Autowired
-    public UserDetailServiceImpl(UserRepository userRepository) {
+    public UserDetailServiceImpl(UserRepository userRepository,
+                                 @Value("${admin.emails}") String adminEmails) {
         this.userRepository = userRepository;
+        this.admins = Arrays.asList(adminEmails.split(";"));
     }
 
     @Override
     @Transactional(readOnly = true)
     public UserDetails loadUserByUsername(String email) {
         final ApplicationUser applicationUser = userRepository.findCustomerByEmail(email).orElseThrow(() -> new UsernameNotFoundException(email));
-        return new User(applicationUser.getUsername(), applicationUser.getPassword(), createAuthorityList("user"));
+        if (admins.contains(email)) {
+            return new User(applicationUser.getUsername(), applicationUser.getPassword(), createAuthorityList("user", "admin"));
+        } else {
+            return new User(applicationUser.getUsername(), applicationUser.getPassword(), createAuthorityList("user"));
+        }
     }
 
 }
--- authentication/src/test/java/authserver/JwtTokenAuthenticationTest.java
+++ authentication/src/test/java/authserver/JwtTokenAuthenticationTest.java
@@ -1,11 +1,15 @@
 package authserver;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.client.ResourceAccessException;
 
+import java.util.List;
 import java.util.UUID;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -13,6 +17,9 @@ import static org.hamcrest.Matchers.is;
 import static org.springframework.http.HttpMethod.POST;
 
 public class JwtTokenAuthenticationTest extends AbstractIntegrationTest {
+
+    @Value("${security.secret}")
+    private String secret;
 
     private BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
@@ -32,7 +39,13 @@ public class JwtTokenAuthenticationTest extends AbstractIntegrationTest {
         ResponseEntity<String> exchange = rest.exchange(BASE_URL, POST, data, String.class);
 
         // Then
-        assertThat(exchange.getHeaders().containsKey("Authentication"), is(true));
+        final Claims claims = Jwts.parser()
+                .setSigningKey(secret.getBytes())
+                .parseClaimsJws(exchange.getHeaders().get("Authentication").get(0))
+                .getBody();
+        final List<String> roles = (List<String>) claims.get("roles");
+        assertThat(roles.size(), is(1));
+        assertThat(roles.get(0), is("user"));
     }
 
     @Test(expected = ResourceAccessException.class)
@@ -82,5 +95,31 @@ public class JwtTokenAuthenticationTest extends AbstractIntegrationTest {
                 "  }";
         HttpEntity<String> data = new HttpEntity<>(json, headers);
         rest.exchange(BASE_URL, POST, data, String.class);
+    }
+
+    @Test
+    public void shouldReturnAdminToken() {
+        // Given
+        String hashedPassword = passwordEncoder.encode("12345");
+        final String sql = "insert into customer (name, email, password, customer_uid) values ('chen', 'admin.one@noodle-monster.co.uk', '" + hashedPassword + "', '"+ UUID.randomUUID() + "')";
+        jdbcTemplate.execute(sql);
+
+        // When
+        String json = "{\n" +
+                "    \"username\": \"admin.one@noodle-monster.co.uk\",\n" +
+                "    \"password\": \"12345\"\n" +
+                "  }";
+        HttpEntity<String> data = new HttpEntity<>(json, headers);
+        ResponseEntity<String> exchange = rest.exchange(BASE_URL, POST, data, String.class);
+
+        // Then
+        final Claims claims = Jwts.parser()
+                .setSigningKey(secret.getBytes())
+                .parseClaimsJws(exchange.getHeaders().get("Authentication").get(0))
+                .getBody();
+        final List<String> roles = (List<String>) claims.get("roles");
+        assertThat(roles.size(), is(2));
+        assertThat(roles.get(0), is("admin"));
+        assertThat(roles.get(1), is("user"));
     }
 }
