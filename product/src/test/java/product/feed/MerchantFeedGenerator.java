ef545b75bae8c7005bc9ca4135c6a50f4cfffa42
jiandongc <jiandong.c@gmail.com>
2020-12-27 17:55:57 +0000

    merchant feed generator

--- product/src/test/java/product/feed/MerchantFeedGenerator.java
+++ product/src/test/java/product/feed/MerchantFeedGenerator.java
@@ -19,14 +19,15 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.Writer;
 import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static java.time.format.DateTimeFormatter.ISO_DATE_TIME;
 
 public class MerchantFeedGenerator extends AbstractRepositoryTest {
 
     private static final String BASE_URL = "https://noodle-monster.co.uk";
-    private static final String BASE_DIRECTORY = "/Users/jiandongchen/Projects";
+    private static final String BASE_DIRECTORY = "/home/jiandong/Project2015";
 
     @Autowired
     private ProductRepository productRepository;
@@ -47,18 +48,18 @@ public class MerchantFeedGenerator extends AbstractRepositoryTest {
                 .map(product -> ProductFeed.builder()
                         .id(product.getSkus().get(0).getSku())
                         .title(product.getName())
-                        .description(product.getShortDescription())
+                        .description(product.getName())
                         .link(BASE_URL + "/products/" + product.getCode())
-                        .price(product.getOriginalPrice() + " GBP")
+                        .price(product.getSkus().get(0).getOriginalPrice() + " GBP")
                         .salePrice(product.getSkus().get(0).getSalePrice() != null ? product.getSkus().get(0).getSalePrice().getPrice().setScale(2, BigDecimal.ROUND_HALF_UP) + " GBP":  null)
-                        .salePriceEffectiveDate(product.getSkus().get(0).getSalePrice() != null ? product.getSkus().get(0).getSalePrice().getStartDate() + "/" + product.getSkus().get(0).getSalePrice().getEndDate() : null)
+                        .salePriceEffectiveDate(product.getSkus().get(0).getSalePrice() != null ? product.getSkus().get(0).getSalePrice().getStartDate().atStartOfDay().format(ISO_DATE_TIME) + "/" + product.getSkus().get(0).getSalePrice().getEndDate().atTime(23, 59, 59).format(ISO_DATE_TIME) : null)
                         .availability(product.getSkus().get(0).getStockQuantity() > 0 ? "in stock" : "out of stock")
                         .imageLink(product.getFirstImageUrl())
                         .gtin(null)
                         .brand(product.getBrand() != null ? product.getBrand().getName() : null)
                         .googleProductCategory(getCategoryNumber(product.getCode()))
                         .shipping(null)
-                        .expirationDate(product.getEndDate() != null ? product.getEndDate().toString() : null)
+                        .expirationDate(product.getEndDate() != null ? product.getEndDate().atTime(23, 59, 59).format(ISO_DATE_TIME) : null)
                         .build())
                 .collect(Collectors.toList());
 
@@ -83,46 +84,32 @@ public class MerchantFeedGenerator extends AbstractRepositoryTest {
     public static class ProductFeed {
 
         @CsvBindByName(column = "id")
-        @CsvBindByPosition(position = 0)
         private String id;
         @CsvBindByName(column = "title")
-        @CsvBindByPosition(position = 1)
         private String title;
         @CsvBindByName(column = "description")
-        @CsvBindByPosition(position = 2)
         private String description;
         @CsvBindByName(column = "link")
-        @CsvBindByPosition(position = 3)
         private String link;
         @CsvBindByName(column = "price")
-        @CsvBindByPosition(position = 4)
         private String price;
         @CsvBindByName(column = "sale_price")
-        @CsvBindByPosition(position = 5)
         private String salePrice;
         @CsvBindByName(column = "sale_price_effective_date")
-        @CsvBindByPosition(position = 6)
         private String salePriceEffectiveDate;
         @CsvBindByName(column = "availability")
-        @CsvBindByPosition(position = 7)
         private String availability;
         @CsvBindByName(column = "image link")
-        @CsvBindByPosition(position = 8)
         private String imageLink;
         @CsvBindByName(column = "gtin")
-        @CsvBindByPosition(position = 9)
         private String gtin;
         @CsvBindByName(column = "brand")
-        @CsvBindByPosition(position = 10)
         private String brand;
         @CsvBindByName(column = "google product category")
-        @CsvBindByPosition(position = 11)
         private String googleProductCategory;
         @CsvBindByName(column = "shipping")
-        @CsvBindByPosition(position = 12)
         private String shipping;
         @CsvBindByName(column = "expiration_date")
-        @CsvBindByPosition(position = 13)
         private String expirationDate;
 
     }
