24d9e25f7cb9f607d02bec7ade99c9932e18fdae
jiandongc <jiandong.c@gmail.com>
2021-06-30 23:08:01 +0100

    email template

--- email/src/main/java/email/data/TemplateData.java
+++ email/src/main/java/email/data/TemplateData.java
@@ -15,13 +15,13 @@ public class TemplateData extends MailData {
     @Builder
     public TemplateData(List<String> sendTo,
                         List<String> bccTo) {
-        super("info@noodle-monster.co.uk", "Noodle Monster", sendTo, bccTo,"Noodle Monster【快报】\uD83D\uDC49 来自新加坡，馬來西亞的味道 | 日本的健康有機面 | 網紅有你一面！");
+        super("info@noodle-monster.co.uk", "Noodle Monster", sendTo, bccTo,"Noodle Monster【快报】\uD83D\uDC49 訂單滿19.99英鎊 | 全英包郵");
     }
 
     @Override
     public String generateText() throws IOException {
         STGroup group = new STGroupDir("template", '$', '$');
-        ST st = group.getInstanceOf("new-product-20210615");
+        ST st = group.getInstanceOf("free-delivery-offer");
         return st.render();
     }
 }
