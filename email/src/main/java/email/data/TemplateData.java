a5719b4f8b6eec63bf22d8ff0261e6823062a725
jiandongc <jiandong.c@gmail.com>
2021-07-17 00:15:33 +0100

    google-pay/apply-pay email

--- email/src/main/java/email/data/TemplateData.java
+++ email/src/main/java/email/data/TemplateData.java
@@ -15,13 +15,13 @@ public class TemplateData extends MailData {
     @Builder
     public TemplateData(List<String> sendTo,
                         List<String> bccTo) {
-        super("info@noodle-monster.co.uk", "Noodle Monster", sendTo, bccTo,"Noodle Monster【快报】\uD83D\uDC49 訂單滿19.99英鎊 | 全英包郵");
+        super("info@noodle-monster.co.uk", "Noodle Monster", sendTo, bccTo,"Noodle Monster【快报】\uD83D\uDC49 支付更便捷，买面更省心！");
     }
 
     @Override
     public String generateText() throws IOException {
         STGroup group = new STGroupDir("template", '$', '$');
-        ST st = group.getInstanceOf("free-delivery-offer");
+        ST st = group.getInstanceOf("now-support-google-pay-apple-pay");
         return st.render();
     }
 }
